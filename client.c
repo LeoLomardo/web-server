57405893919ead24f2519c5a31088d6f81acae56
Leo Lomardo <leoland771@gmail.com>
2024-06-05 16:55:09 -0300

    final touch's

--- client.c
+++ client.c
@@ -1,54 +1,51 @@
 #include "client.h"
 
-
 extern LogBuffer log_buffer;
 extern Command *command;
 extern StatsInfo stats;
 
-void *clientRequest(void *client_sockfd) {
+void *clientRequest(void *client_sockfd){
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
 
     int sock = *(int *)client_sockfd;
     free(client_sockfd);
     
-    char buffer[MAX_BUFFER_SIZE] = {0};
+    char buffer[MAX_BUFFER_SIZE] ={0};
     
     int read_size = read(sock, buffer, MAX_BUFFER_SIZE-1);
-    if (read_size > 0) {
+    if(read_size > 0){
         buffer[read_size] = '\0';
         printf(" %s\n", buffer);
-    } else {
+    }else{
         fprintf(stderr, "[SERVER] - Read failed\n");
         close(sock);
         return NULL;
     }
 
-    char method[10] = {0}, path[255] = {0}, protocol[10] = {0};
+    char method[10] ={0}, path[255] ={0}, protocol[10] ={0};
     sscanf(buffer, "%s %s %s", method, path, protocol);
     
     char full_path[256] = ".";
     strncat(full_path, path, sizeof(full_path) - strlen(full_path) - 1);
-
     printf("[SERVER] - Requested path: %s\n", full_path);
 
-    // Abrir o arquivo em modo binário
     FILE *inputFile = fopen(full_path, "rb");
 
-    if (inputFile == NULL) {
+    if(inputFile == NULL){
         const char *error_message = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nFile not found.\n";
         write(sock, error_message, strlen(error_message));
-        snprintf(buffer, 1024, "%04d-%02d-%02d %02d:%02d:%02d 404 Not Found: %s\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec, full_path);
+        snprintf(buffer, MAX_BUFFER_SIZE, "%04d-%02d-%02d %02d:%02d:%02d 404 Not Found: %s\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec, full_path);
         LEntry(&log_buffer, buffer);
         close(sock);
         return NULL;
     } 
     char *contentExtention = strrchr(full_path, '.');
-    char header[MAX_BUFFER_SIZE] = {0};
-    char content_type[80] = {0};
+    char header[MAX_BUFFER_SIZE] ={0};
+    char content_type[80] ={0};
 
     struct stat lastChange;
-    if (stat(full_path, &lastChange) == -1) {
+    if(stat(full_path, &lastChange) == -1){
         fprintf(stderr, "Erro ao obter informações do arquivo\n");
         fclose(inputFile);
         close(sock);
@@ -56,55 +53,55 @@ void *clientRequest(void *client_sockfd) {
     }
 
     int content_length = lastChange.st_size;
-    char etag[64] = {0};
 
-    snprintf(etag, sizeof(etag), "W/\"%lx-%lx\"", lastChange.st_size, lastChange.st_mtime);
-
-    if (contentExtention != NULL) {
-        if (strcmp(contentExtention, ".html") == 0 || strcmp(contentExtention, ".htm") == 0) {
+    if(contentExtention != NULL){
+        if(strcmp(contentExtention, ".html") == 0 || strcmp(contentExtention, ".htm") == 0){
             stats.html_count++;
             snprintf(content_type, sizeof(content_type), "text/html");
-            
-            // Analisar o conteúdo do arquivo HTML para contar as imagens e arquivos de texto
+            /**
+             * Utilizei SEEK_END para poder calcular o tamanho do arquivo e alocar a memoria necessaria 
+             * Depois, para nao precisar utilizar rewind, utilizei SEEK_SET para voltar o ponteiro para o comeco do arquivo
+             * Abaixo eu utilizo a variavel content_length para armazenar o tamanho do arquivo, porem quando eu utilizava ela dentro do if,
+             * tinha problemas, entao preferi criar novamente utilizando essa maneira.
+             */
             fseek(inputFile, 0, SEEK_END);
             long file_size = ftell(inputFile);
             fseek(inputFile, 0, SEEK_SET);
             
             char *file_content = malloc(file_size + 1);
-            if (file_content) {
+            if(file_content){
                 fread(file_content, 1, file_size, inputFile);
                 file_content[file_size] = '\0';
 
                 char *img_tag = file_content;
                 char *txt_tag = file_content;
 
-                while (img_tag || txt_tag) {
-                    if (img_tag) {
+                while (img_tag || txt_tag){
+                    if(img_tag){
                         img_tag = strstr(img_tag, "<img ");
-                        if (img_tag) {
+                        if(img_tag){
                             stats.image_count++;
-                            img_tag += 5; // Avançar para além de "<img "
+                            img_tag += 5;
                         }
                     }
-                    if (txt_tag) {
+                    if(txt_tag){
                         txt_tag = strstr(txt_tag, ".txt");
-                        if (txt_tag) {
+                        if(txt_tag){
                             stats.text_count++;
-                            txt_tag += 4; // Avançar para além de ".txt"
+                            txt_tag += 4;
                         }
                     }
                 }
-
                 free(file_content);
             }
             rewind(inputFile); 
-        } else if (strcmp(contentExtention, ".jpg") == 0 || strcmp(contentExtention, ".jpeg") == 0) {
+        }else if(strcmp(contentExtention, ".jpg") == 0 || strcmp(contentExtention, ".jpeg") == 0){
             stats.image_count++;
             snprintf(content_type, sizeof(content_type), "image/jpeg");
-        } else if (strcmp(contentExtention, ".png") == 0) {
+        }else if(strcmp(contentExtention, ".png") == 0){
             stats.image_count++;
             snprintf(content_type, sizeof(content_type), "image/png");
-        } else {
+        }else{
             stats.text_count++;
             snprintf(content_type, sizeof(content_type), "text/plain");
         }
@@ -114,7 +111,6 @@ void *clientRequest(void *client_sockfd) {
     char lastmodbuf[128];
     strftime(timebuf, sizeof(timebuf), "%a, %d %b %Y %H:%M:%S GMT", gmtime(&now));
     strftime(lastmodbuf, sizeof(lastmodbuf), "%a, %d %b %Y %H:%M:%S GMT", gmtime(&lastChange.st_mtime));
-
     snprintf(header, sizeof(header), 
         "HTTP/1.1 200 OK\r\n"
         "Server:FerramentasUnixPUC/1.1\r\n"
@@ -124,21 +120,20 @@ void *clientRequest(void *client_sockfd) {
         "Content-Type:%s\r\n\r\n", 
         lastmodbuf, content_length, timebuf, content_type);
 
-    if (write(sock, header, strlen(header)) == -1) {
+    if(write(sock, header, strlen(header)) == -1){
         fprintf(stderr, "[SERVER] - Failed to send header");
         fclose(inputFile);
         close(sock);
         return NULL;
     }
-    //write(sock, header, strlen(header));
     printf(" %s\n", header);
 
     char ret_buffer[MAX_BUFFER_SIZE] = {0};
 
     int bytes_read;
 
-    while ((bytes_read = fread(ret_buffer, 1, sizeof(ret_buffer), inputFile)) > 0) {
-       if (write(sock, ret_buffer, bytes_read) == -1) {
+    while ((bytes_read = fread(ret_buffer, 1, sizeof(ret_buffer), inputFile)) > 0){
+       if(write(sock, ret_buffer, bytes_read) == -1){
             fprintf(stderr, "[SERVER] - Failed to send file content\n");
             fclose(inputFile);
             close(sock);
--- commandHandle.c
+++ commandHandle.c
@@ -1,11 +1,9 @@
 #include "commandHandle.h"
 
-Command *extractCommandOptions(int argc, char *argv[]) {
-    int opt;
-
+Command *extractCommandOptions(int argc, char *argv[]){
     Command *command = (Command *)malloc(sizeof(Command));
-    if (command == NULL) {
-        fprintf(stderr, "[CONSOLE] - Error trying to malloc command.\n");
+    if(command == NULL){
+        fprintf(stderr, "commandHandler - Error alocating memory.\n");
         exit(EXIT_FAILURE);
     }
 
@@ -15,33 +13,33 @@ Command *extractCommandOptions(int argc, char *argv[]) {
     command->statsFilename = NULL;
     command->rootDir = NULL;
 
-    if (argc < 2) {
-        printf("[CONSOLE] - Please start the server using the following usage below.\n");
-        printf("Usage: %s -p <port> -l <logfile> -s <statsfile> -r <rootdir> -b\n", argv[0]);
+    if(argc < 2){
+        printf("commandHandler -  Error number of arguments\n");
+        printf("Format: %s -p <port> -l <logfile> -s <statsfile> -r <rootdir> -b\n", argv[0]);
         free(command);
         exit(EXIT_FAILURE);
     }
-
-    while ((opt = getopt(argc, argv, "p:l:s:r:b")) != -1) {
-        switch (opt) {
+    int opt;
+    while((opt = getopt(argc, argv, "p:l:s:r:b")) != -1){
+        switch (opt){
             case 'p':
                 command->port = atoi(optarg);
                 break;
             case 'l':
-                printf("[CONSOLE] - Log file name is %s\n", optarg);
+                printf("commandHandler - Log file name is %s\n", optarg);
                 command->logFilename = (char *)malloc(strlen(optarg) + 1);
-                if (command->logFilename == NULL) {
-                    fprintf(stderr, "[CONSOLE] - Error trying to malloc logFilename.\n");
+                if(command->logFilename == NULL){
+                    fprintf(stderr, "commandHandler - alocating memory for logFilename.\n");
                     free(command);
                     exit(EXIT_FAILURE);
                 }
                 strcpy(command->logFilename, optarg);
                 break;
             case 's':
-                printf("[CONSOLE] - Stats file name is %s\n", optarg);
+                printf("commandHandler - Stats file name is %s\n", optarg);
                 command->statsFilename = (char *)malloc(strlen(optarg) + 1);
-                if (command->statsFilename == NULL) {
-                    fprintf(stderr, "[CONSOLE] - Error trying to malloc statsFilename.\n");
+                if(command->statsFilename == NULL){
+                    fprintf(stderr, "commandHandler - Error alocating memory for statsFilename.\n");
                     free(command->logFilename);
                     free(command);
                     exit(EXIT_FAILURE);
@@ -49,10 +47,10 @@ Command *extractCommandOptions(int argc, char *argv[]) {
                 strcpy(command->statsFilename, optarg);
                 break;
             case 'r':
-                printf("[CONSOLE] - Root directory is %s \n", optarg);
+                printf("commandHandler - Root directory is %s \n", optarg);
                 command->rootDir = (char *)malloc(strlen(optarg) + 1);
-                if (command->rootDir == NULL) {
-                    fprintf(stderr, "[CONSOLE] - Error trying to malloc rootDir.\n");
+                if(command->rootDir == NULL){
+                    fprintf(stderr, "commandHandler - Error alocating memory for rootDir.\n");
                     free(command->logFilename);
                     free(command->statsFilename);
                     free(command);
@@ -61,7 +59,7 @@ Command *extractCommandOptions(int argc, char *argv[]) {
                 strcpy(command->rootDir, optarg);
                 break;
             case 'b':
-                printf("[CONSOLE] - Background running set\n");
+                printf("commandHandler - Running wit background flag == 1\n");
                 command->background = 1;
                 break;
             default:
@@ -73,7 +71,6 @@ Command *extractCommandOptions(int argc, char *argv[]) {
                 exit(EXIT_FAILURE);
         }
     }
-
     return command;
 }
 
